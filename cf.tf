edd2cf83c80228b86a607c5e6e16b3cc2a2b4fff
peakyblinder0509 <connect2ikram509@gmail.com>
2026-06-26 10:17:34 +0530

    Add CloudFront distribution resource configuration

---
+++ cf.tf
@@ -0,0 +1,34 @@
+resource "aws_cloudfront_distribution" "assesment" {
+  origin {
+    domain_name = aws_s3_bucket.main.bucket_regional_domain_name
+    origin_id    = "S3-origin"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
