c9b6f2d9c98200dbd3c2246448f0ad594332eccd
peakyblinder0509 <connect2ikram509@gmail.com>
2026-06-26 11:51:06 +0530

    Update CloudFront origin to use S3 website endpoint

--- cf.tf
+++ cf.tf
@@ -1,7 +1,14 @@
 resource "aws_cloudfront_distribution" "assesment" {
   origin {
-    domain_name = aws_s3_bucket.main.bucket_regional_domain_name
-    origin_id    = "S3-origin"
+    domain_name = aws_s3_bucket_website_configuration.assesment.website_endpoint
+    origin_id   = "S3-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port              = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
 
   enabled             = true
@@ -9,7 +16,7 @@ resource "aws_cloudfront_distribution" "assesment" {
 
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
+    cached_methods          = ["GET", "HEAD"]
     target_origin_id       = "S3-origin"
     viewer_protocol_policy = "redirect-to-https"
 
