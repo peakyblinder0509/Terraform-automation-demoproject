78184b77971a32543e2733e576075ba877fc578d
peakyblinder0509 <connect2ikram509@gmail.com>
2026-06-26 11:50:19 +0530

    Add S3 bucket policy for public read access

--- s3.tf
+++ s3.tf
@@ -1,18 +1,20 @@
-    resource "aws_s3_bucket" "main" {
+resource "aws_s3_bucket" "main" {
   bucket = "ikramapzz-assesment-s3"
-  
+
   tags = {
     Name = "ikramapzz-s3-bucket"
   }
 }
+
 resource "aws_s3_bucket_public_access_block" "main" {
   bucket = aws_s3_bucket.main.id
-  
+
   block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = false
+  ignore_public_acls       = false
   restrict_public_buckets = false
 }
+
 resource "aws_s3_bucket_website_configuration" "assesment" {
   bucket = aws_s3_bucket.main.id
 
@@ -23,4 +25,23 @@ resource "aws_s3_bucket_website_configuration" "assesment" {
   error_document {
     key = "index.html"
   }
+}
+
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.main.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.main]
 }
